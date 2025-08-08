d25f1d531d862d8a54014ca4a35ce0945ff498ce
Adryano Junior <jusamsung10@gmail.com>
2025-08-08 15:30:11 -0300

    ensuring the login API works

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,16 @@
+describe('Teste da API - Login', () => {
+
+  it('Deve auteenticar com sucesso e retornar um token', () => {
+    cy.request({
+      method: 'POST',
+      url: 'login',
+      body: {
+          "email": "fulano@qa.com",
+          "password": "teste"
+}
+    }).should((response) =>{
+      expect(response.status).equal(200)
+      expect(response.body.message).equal('Login realizado com sucesso')
+    })
+  })
+})
